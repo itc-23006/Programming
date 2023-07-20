c96c83ff71d95da6ee1810f2ab02d2104d38fd72
itc-23006 <n23006@std.it-college.ac.jp>
2023-07-20 05:47:07 +0000

    Q4_2 Q4_3

---
+++ CHAPTER04/Q4_3_1.py
@@ -0,0 +1,8 @@
+def concat_words(*args, separator="."):
+    return separator.join(args)
+
+
+# 実行
+print(concat_words("a", "b", "c", "d", separator="_"))
+
+print(concat_words("4_choume", "Minatoku", "Tokyo", "Japan", separator=" "))
