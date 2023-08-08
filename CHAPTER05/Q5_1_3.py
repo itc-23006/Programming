bb184cfa56677ccc774c8b509b9885d87fb90336
itc-23006 <n23006@std.it-college.ac.jp>
2023-08-08 06:02:59 +0000

    Q5_1_1.py Q5_1_2.py Q5_1_3.py

---
+++ CHAPTER05/Q5_1_3.py
@@ -0,0 +1,8 @@
+queue = []
+queue.append(1)  # Append 1 to the stack
+print("stack:", queue)
+queue.append(2)  # Append 2 to the stack
+print("stack:", queue)
+print("pop 1st value:", queue.pop())  # Pop the first value (2) from the stack
+print("pop 2nd value:", queue.pop())  # Pop the second value (1) from the stack
+print("stack:", queue)
